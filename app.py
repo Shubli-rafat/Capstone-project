dbe24a6cc49f06d89dcf211b7ec9075acd449c1d
Shubli-rafat <shuboo.25sep@yahoo.com>
2022-11-14 20:39:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h2>Hello WORLD! My name is Shubli Rafat and this is my Capstone Project<h2h>"
+    return "<h2>Hello WORLD! My name is Shubli Rafat and this is my Capstone Project<h2>
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
